7d839dbdd4a66f1e5650b5a5a9cd48e0320df25d
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-03-07 13:55:33 +0100

    updated the instance attribute

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,12 @@ class BaseModel:
         """Initialize a new instance of the BaseModel class"""
         if kwargs:
             for key, value in kwargs.items():
-               if key == '__class__':
-                   continue
-               setattr(self, key, value)
-
-            self.created_at = datetime.strptime(
-                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
- 
+                if key in ('created_at','updated_at'):
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+                elif key != '__class__':
+                    setattr(self, key, value)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -41,3 +38,4 @@ class BaseModel:
         result_copy['created_at'] = self.created_at.isoformat()
         result_copy['updated_at'] = self.updated_at.isoformat()
         return result_copy
+
